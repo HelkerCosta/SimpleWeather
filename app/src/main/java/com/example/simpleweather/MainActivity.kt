7412ec2da3d58d6340ad31e7509755a8595ccadf
helker <helkergcosta@gmail.com>
2021-06-22 22:31:31 -0400

    Added image

--- app/src/main/java/com/example/simpleweather/MainActivity.kt
+++ app/src/main/java/com/example/simpleweather/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.simpleweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setUpView(){
+        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+
     }
 }
