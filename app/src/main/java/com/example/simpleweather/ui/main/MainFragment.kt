0664064a76c6351ac2e9e42b3706b7e717aac8bd
Helker costa <helkergcosta@gmail.com>
2021-11-04 20:47:14 -0400

    delete this

--- app/src/main/java/com/example/simpleweather/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/simpleweather/ui/main/MainFragment.kt
@@ -1,7 +1,24 @@
 package com.example.simpleweather.ui.main
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.fragment.app.Fragment
 import com.example.simpleweather.R
 
 class MainFragment: Fragment(R.layout.fragment_main) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val img = view.findViewById<View>(R.id.imageView)
+        val springAnimation = SpringAnimation(img,DynamicAnimation.TRANSLATION_Y, 300f)
+
+        springAnimation.setStartVelocity(100f)
+        springAnimation.start()
+    }
 }
